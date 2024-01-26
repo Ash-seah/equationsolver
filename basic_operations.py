7ec9e7e535541c558c1986acbc6369b8b1e1be34
Ash-seah <ash.seah.1@gmail.com>
2024-01-26 11:45:05 +0330

    division progress

--- basic_operations.py
+++ basic_operations.py
@@ -6,7 +6,14 @@ def add(a, b):
         i += 1
     return addition
 
-def subtract(a, b):
+def subtract(a, c):
+    b = c
+    print(len(a), len(b))
+    while len(a) != len(b):
+        print(len(a), len(b))
+        b = list(b[::-1])
+        b.append(0)
+        b = list(b[::-1])
     i = 0
     subtraction = []
     while i < len(a):
@@ -20,26 +27,29 @@ def multiply(a, b):
     i = 0
     while i < len(a):
         j = 0
-        while j < len(j):
+        while j < len(b):
             multiply[i+j] += a[i] * b[j]
             j += 1
         i += 1
-    return multiply[::-1]
+    return trim(list(multiply[::-1]))
 
 # add a normalization function that matches the lengths of 2 polynomials
 def divide(a, b):
     curr_poly = a
     quotient = []
-    while len(b) <= len(curr_poly):
-        quotient.append(curr_poly[0]/b[0])
-        curr_poly= trim(subtract(curr_poly, int(round(curr_poly[0]/b[0])) * b))
-    remainder = curr_poly
+    i = 0
+    while i < len(a) - len(b) + 1:
+        temp = [0] * (len(a) - len(b) + 1)
+        temp[i] += int(curr_poly[i]/b[0])
+        curr_poly = subtract(curr_poly, multiply(temp, b))
+        quotient.append(int(curr_poly[i]/b[0]))
+        i += 1
     
-    return [quotient, remainder]
+    return quotient
 
 def trim(a):
     while a[0] == 0:
         a.pop(0)
     return a
 
-print(divide([4,0,2,0], [2,1]))
+print(divide([2,9,14,5], [2,1]))
